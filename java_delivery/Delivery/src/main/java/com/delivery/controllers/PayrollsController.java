8dbc431197ced38f61cfb48d28ea3aa352d1a577
cliske94 <cody.liske01@gmail.com>
2020-05-30 01:06:00 -0400

    Finished CRUD implementation for rest of tables

--- java_delivery/Delivery/src/main/java/com/delivery/controllers/PayrollsController.java
+++ java_delivery/Delivery/src/main/java/com/delivery/controllers/PayrollsController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +36,34 @@ public class PayrollsController {
 		Payroll foundPayroll = dao.findById(id).orElse(null);
 		if(foundPayroll == null) return ResponseEntity.notFound().header("Payroll", "Nothing found with that id").build();
 		return ResponseEntity.ok(foundPayroll);
+	}
+	
+	@PostMapping("/payroll")
+	public ResponseEntity<Payroll> postPayroll(@RequestBody Payroll payroll) {
+		Payroll createdPayroll = dao.save(payroll);
+		return ResponseEntity.ok(createdPayroll);
+	}
+	
+	@DeleteMapping("/payroll/{transaction_id}")
+	public ResponseEntity<Payroll> deletePayroll(@PathVariable(value="transaction_id") Integer id) {
+		Payroll foundPayroll = dao.findById(id).orElse(null);
+		if (foundPayroll == null) return ResponseEntity.notFound().header("Payroll", "Nothing found with thatt id").build();
+		else dao.delete(foundPayroll);
+		return ResponseEntity.ok().build();
+	}
+	
+	@PutMapping("/payroll/{transaction_id}")
+	public ResponseEntity<Payroll> updatePayroll(@RequestBody Payroll payroll, @PathVariable(value="transaction_id") Integer id) {
+		Payroll foundPayroll = dao.findById(id).orElse(null);
+		if (foundPayroll == null) return ResponseEntity.notFound().header("Payroll", "Nothing found with that id").build();
+		else {
+			foundPayroll.setGross_pay(payroll.getGross_pay());
+			foundPayroll.setHours_worked(payroll.getHours_worked());
+			foundPayroll.setTaxes_taken(payroll.getTaxes_taken());
+			foundPayroll.setTimestamp(payroll.getTimestamp());
+			foundPayroll.setYTD_pay(payroll.getYTD_pay());
+			dao.save(foundPayroll);
+		}
+		return ResponseEntity.ok().header("Payroll", "Properly updated payroll record").build();
 	}
 }
